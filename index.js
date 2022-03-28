06f971dd70adf933cd4964612f535d7ffed9d680
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-28 00:35:07 -0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { Messages } = require('./models')
+const { Messages } = require('./models');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/send', async (req, res) => {
