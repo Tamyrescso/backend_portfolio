40b944a587ac339f0b0429a76292cef5e33e752f
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-03-28 00:19:37 -0300

    faz o código para requisição

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { Messages } = require('./models')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/send', async (req, res) => {
+
+  const { name, email, content } = req.body;
+
+  const message = await Messages.create({ name, email, content });
+
+  if(!message) return res.status(500).json({ "message": "Erro ao enviar mensagem, tente novamente mais tarde." })
+
+  return res.status(201).json({"message": "Mensagem enviada com sucesso!"});
+});
+
+app.listen(port, () => console.log(`Servidor online na porta ${port}`));
