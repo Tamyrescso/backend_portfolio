6864cecfea1f0a366ea27f1ef9be511376a3dc8a
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-04-02 13:01:26 -0300

    add condicional de linguagem para mensagem de retorno

--- index.js
+++ index.js
@@ -14,13 +14,15 @@ app.get('/send', async (_req,res) => {
 
 app.post('/send', async (req, res) => {
 
-  const { name, email, content } = req.body;
+  const { name, email, content, language } = req.body;
 
   const message = await Messages.create({ name, email, content });
 
   if(!message) return res.status(500).json({ "message": "Erro ao enviar mensagem, tente novamente mais tarde." })
 
-  return res.status(201).json({"message": "Mensagem enviada com sucesso!"});
+  const returnMessage = language === 'pt' ? "Mensagem enviada com sucesso!" : "Message sent successfully!"
+
+  return res.status(201).json({ "message": returnMessage });
 });
 
 app.listen(port, () => console.log(`Servidor online na porta ${port}`));
